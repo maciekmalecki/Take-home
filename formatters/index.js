bf091f7be428533e9639d95ecbb2caad33aa638b
maciekmalecki <mathumalecki@Mathus-MacBook-Pro.local>
2023-11-13 10:40:45 +0100

    Aggregate and export formatter functions in formatters/index.js

---
+++ formatters/index.js
@@ -0,0 +1,13 @@
+const basketballFormatter = require('./basketballFormatter');
+const handballFormatter = require('./handballFormatter');
+const soccerFormatter = require('./soccerFormatter');
+const tennisFormatter = require('./tennisFormatter');
+const volleyballFormatter = require('./volleyballFormatter');
+
+module.exports = {
+  basketballFormatter,
+  handballFormatter,
+  soccerFormatter,
+  tennisFormatter,
+  volleyballFormatter,
+};
--- formatters/tennisFormatter.js
+++ formatters/tennisFormatter.js
@@ -2,10 +2,6 @@ const formatTennis = match => {
   const regex = /([0-9]+:[0-9]+)(?:,([0-9]+:[0-9]+))*?/;
   const scores = match.score.match(regex);
 
-  if (!scores) {
-      return { error: 'Invalid score format for tennis match' };
-  }
-
   const overallScore = scores[1];
   const setScores = scores[0].split(',').slice(1);
   const formattedSetScores = setScores.map((score, index) => `set${index + 1} ${score}`).join(', ');
